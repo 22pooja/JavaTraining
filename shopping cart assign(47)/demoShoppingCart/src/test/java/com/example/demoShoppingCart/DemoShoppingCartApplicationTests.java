4a4c468b8ec75c608af13b1d452291b4942f54c4
22pooja <67100800+22pooja@users.noreply.github.com>
2022-05-31 15:02:55 +0530

    initial commit
    Shopping Cart Assignment

---
+++ shopping cart assign(47)/demoShoppingCart/src/test/java/com/example/demoShoppingCart/DemoShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
